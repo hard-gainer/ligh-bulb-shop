86666231966d3ba799747ac60800430832576eaf
alex <aleksey.rytov@gmail.com>
2026-04-26 15:02:41 +0300

    add additional exception

--- order_service/exceptions.py
+++ order_service/exceptions.py
@@ -2,6 +2,8 @@ from fastapi import HTTPException
 
 ORDER_NOT_FOUND_DETAIL = "Order not found"
 ORDER_ACCESS_FORBIDDEN_DETAIL = "You have no access to this order"
+PRODUCT_NOT_FOUND_DETAIL = "Product not found"
+PRODUCT_SERVICE_UNAVAILABLE_DETAIL = "Product service is unavailable"
 
 
 def order_not_found() -> HTTPException:
@@ -10,3 +12,14 @@ def order_not_found() -> HTTPException:
 
 def order_access_forbidden() -> HTTPException:
     return HTTPException(status_code=403, detail=ORDER_ACCESS_FORBIDDEN_DETAIL)
+
+
+def product_not_found(product_id: int) -> HTTPException:
+    return HTTPException(
+        status_code=404,
+        detail=f"{PRODUCT_NOT_FOUND_DETAIL}: {product_id}",
+    )
+
+
+def product_service_unavailable() -> HTTPException:
+    return HTTPException(status_code=503, detail=PRODUCT_SERVICE_UNAVAILABLE_DETAIL)
